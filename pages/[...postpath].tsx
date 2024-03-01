90f5d25fcf48d237d05740862823dc61de1bdbba
kylielee505 <161225403+kylielee505@users.noreply.github.com>
2024-03-02 03:38:08 +0530

    Update [...postpath].tsx

--- pages/[...postpath].tsx
+++ pages/[...postpath].tsx
@@ -13,18 +13,15 @@ export const getServerSideProps: GetServerSideProps = async (ctx) => {
 	const fbclid = ctx.query.fbclid;
 
 	// redirect if facebook is the referer or request contains fbclid
-		if (referringURL?.includes('facebook.com') || fbclid) {
-
+	if (referringURL?.includes('facebook.com') || fbclid) {
 		return {
 			redirect: {
 				permanent: false,
-				destination: `${
-					`https://wanderlusttest.000webhostapp.com/
-` + encodeURI(path as string)
-				}`,
+				destination: `https://wanderlusttest.000webhostapp.com/${encodeURI(path as string)}`,
 			},
 		};
-		}
+	}
+
 	const query = gql`
 		{
 			post(id: "/${path}/", idType: URI) {
